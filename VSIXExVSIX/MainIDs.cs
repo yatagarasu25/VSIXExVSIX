ba6b9022977618d1198734d5e960b7200edbcc6d
Igor Karatayev <igor.cwer@gmail.com>
2020-04-25 13:49:44 +0300

    Simple package.

---
+++ VSIXExVSIX/MainIDs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using VSIXEx.Attributes;
+
+namespace VSIXExVSIX
+{
+	[GuidSymbols]
+	public static class GuidSymbols
+	{
+		[GuidSymbol]
+		public const string guidMainPackage = "87ac2de8-7288-4c92-8d2e-8827ce7e71c9";
+		[GuidSymbol]
+		public const string guidMainPackageCmdSet = "3767ee58-dd86-41eb-93f7-8896274b8c7d";
+		[GuidSymbol]
+		public const string guidImages = "d1840211-2031-4c96-b606-670e494a093f";
+
+		public static readonly Guid MainPackage = new Guid(guidMainPackage);
+		public static readonly Guid MainCommandSet = new Guid(guidMainPackageCmdSet);
+	}
+
+}
---
+++ VSIXExVSIX/MainPackage.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+using VSIXEx;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIXExVSIX
+{
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[ProvideAutoLoad(UIContextGuids.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
+	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+	[Guid(GuidSymbols.guidMainPackage)]
+	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
+	public class MainPackage : BaseAsyncPackage
+	{
+		public MainPackage()
+		{
+			// Inside this method you can place any initialization code that does not require
+			// any Visual Studio service because at this point the package object is created but
+			// not sited yet inside Visual Studio environment. The place to do all the other
+			// initialization is the Initialize method.
+		}
+
+		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+		{
+			await base.InitializeAsync(cancellationToken, progress);
+
+			// When initialized asynchronously, the current thread may be a background thread at this point.
+			// Do any initialization that requires the UI thread after switching to the UI thread.
+			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+		}
+	}
+}
